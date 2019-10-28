360f63326ce6c869524735945ca08f41d7da68de
NDECROIX <nicolas.decroixx@gmail.com>
2019-10-28 15:07:15 +0100

    Create BaseActivity with Firebase functions

---
+++ Go4Lunch/app/src/main/java/com/decroix/nicolas/go4lunch/base/BaseActivity.java
@@ -0,0 +1,60 @@
+package com.decroix.nicolas.go4lunch.base;
+
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    /**
+     * Get the current user logged
+     * @return Firebase user
+     */
+    @Nullable
+    protected FirebaseUser getCurrentUser() {
+        return FirebaseAuth.getInstance().getCurrentUser();
+    }
+
+    /**
+     * Get the current user ID
+     * @return Firebase user ID
+     */
+    @Nullable
+    protected String getCurrentUserID() {
+        if (getCurrentUser() != null) {
+            return getCurrentUser().getUid();
+        }
+        return null;
+    }
+
+    /**
+     * Returns true if a user is currently logged in
+     * @return True if the user is logged in
+     */
+    protected Boolean isCurrentUserLogged() {
+        return (this.getCurrentUser() != null);
+    }
+
+
+    /**
+     * Handled the failure from a Task
+     * @return On failure listener
+     */
+    protected OnFailureListener onFailureListener(String message) {
+        return e -> Toast.makeText(getApplicationContext(),
+                message, Toast.LENGTH_LONG).show();
+    }
+
+    /**
+     * Display the message passed as a parameter
+     * @param message Message that we show to the user
+     */
+    protected void showMessage(String message){
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+}
