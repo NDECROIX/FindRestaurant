6e71c06a836933082060fde6890ea2622fe2330f
NDECROIX <nicolas.decroixx@gmail.com>
2019-10-28 18:54:55 +0100

    MapViewFragment : inflate Google map fragment

--- Go4Lunch/app/src/main/java/com/decroix/nicolas/go4lunch/controller/fragments/MapViewFragment.java
+++ Go4Lunch/app/src/main/java/com/decroix/nicolas/go4lunch/controller/fragments/MapViewFragment.java
@@ -1,32 +1,70 @@
 package com.decroix.nicolas.go4lunch.controller.fragments;
 
 
+import android.location.Location;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.decroix.nicolas.go4lunch.BuildConfig;
 import com.decroix.nicolas.go4lunch.R;
+import com.decroix.nicolas.go4lunch.base.BaseFragment;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.net.PlacesClient;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.ButterKnife;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MapViewFragment extends Fragment {
+public class MapViewFragment extends BaseFragment
+        implements OnMapReadyCallback {
 
+
+    private GoogleMap mMap;
+    private PlacesClient placesClient;
+    private List<Marker> markers;
+    private Location mLastKnownLocation;
 
     public MapViewFragment() {
         // Required empty public constructor
     }
 
-
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map_view, container, false);
+        View view = inflater.inflate(R.layout.fragment_map_view, container, false);
+        ButterKnife.bind(this, view);
+        SupportMapFragment supportMapFragment = ((SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.fragment_map_view));
+        if (supportMapFragment != null) {
+            supportMapFragment.getMapAsync(this);
+        }
+        return view;
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Places.initialize(getFragmentContext(), BuildConfig.ApiKey);
+        placesClient = Places.createClient(getFragmentContext());
+        markers = new ArrayList<>();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+    }
 }
