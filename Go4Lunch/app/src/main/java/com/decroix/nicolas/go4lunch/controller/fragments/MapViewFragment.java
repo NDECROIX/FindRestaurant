0dfd7cbd2edefd6de60ed74132e841085543bf08
NDECROIX <nicolas.decroixx@gmail.com>
2019-10-28 19:22:01 +0100

    Display the marker on restaurants next to the device location

--- Go4Lunch/app/src/main/java/com/decroix/nicolas/go4lunch/controller/fragments/MapViewFragment.java
+++ Go4Lunch/app/src/main/java/com/decroix/nicolas/go4lunch/controller/fragments/MapViewFragment.java
@@ -1,7 +1,8 @@
 package com.decroix.nicolas.go4lunch.controller.fragments;
 
-
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.location.Location;
 import android.os.Bundle;
 
@@ -17,17 +18,26 @@ import android.view.ViewGroup;
 
 import com.decroix.nicolas.go4lunch.BuildConfig;
 import com.decroix.nicolas.go4lunch.R;
+import com.decroix.nicolas.go4lunch.api.RestaurantHelper;
 import com.decroix.nicolas.go4lunch.base.BaseFragment;
+import com.decroix.nicolas.go4lunch.models.Restaurant;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.Task;
 import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.model.PlaceLikelihood;
+import com.google.android.libraries.places.api.net.FindCurrentPlaceRequest;
+import com.google.android.libraries.places.api.net.FindCurrentPlaceResponse;
 import com.google.android.libraries.places.api.net.PlacesClient;
 
 import java.util.ArrayList;
@@ -44,7 +54,7 @@ import static android.Manifest.permission.ACCESS_FINE_LOCATION;
  * A simple {@link Fragment} subclass.
  */
 public class MapViewFragment extends BaseFragment
-        implements OnMapReadyCallback {
+        implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
 
 
     private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 898;
@@ -146,4 +156,82 @@ public class MapViewFragment extends BaseFragment
                 }).addOnFailureListener(this.onFailureListener(getString(R.string.afl_get_location)));
     }
 
+    /**
+     * Find all restaurants around the device position and display a marker based on it
+     */
+    private void getCurrentPlaces() {
+        List<Place> places = new ArrayList<>();
+        // Use the builder to create a FindCurrentPlaceRequest.
+        FindCurrentPlaceRequest request = FindCurrentPlaceRequest.builder(PLACES_FIELDS).build();
+        // Call findCurrentPlace and handle the response (first check that the user has granted permission).
+        if (EasyPermissions.hasPermissions(getFragmentContext(), ACCESS_FINE_LOCATION)) {
+            Task<FindCurrentPlaceResponse> placeResponse = placesClient.findCurrentPlace(request);
+            placeResponse.addOnCompleteListener(task -> {
+                if (task.isSuccessful() && task.getResult() != null) {
+                    FindCurrentPlaceResponse response = task.getResult();
+                    for (PlaceLikelihood placeLikelihood : response.getPlaceLikelihoods()) {
+                        List<Place.Type> types = placeLikelihood.getPlace().getTypes();
+                        if (types != null && types.contains(Place.Type.RESTAURANT)) {
+                            places.add(placeLikelihood.getPlace());
+                        }
+                    }
+                    if (!places.isEmpty()) {
+                        addMarkerColor(places);
+                    }
+                }
+            }).addOnFailureListener(this.onFailureListener(getString(R.string.afl_fetch_place)));
+        } else {
+            getAccessFineLocationPermission();
+        }
+    }
+
+    /**
+     * Create and add a marker on the map
+     * @param places restaurant where add the marker
+     */
+    private void addMarkerColor(@NonNull List<Place> places) {
+        for (Place place : places) {
+            if (!markerExist(place) && place.getLatLng() != null)
+                RestaurantHelper.getRestaurant(place.getId()).addOnCompleteListener(doc -> {
+                    int markerResource = R.drawable.ic_marker_white;
+                    if (doc.isSuccessful() && doc.getResult() != null) {
+                        Restaurant restaurant = doc.getResult().toObject(Restaurant.class);
+                        if (restaurant != null && !restaurant.getUsers().isEmpty())
+                            markerResource = R.drawable.ic_marker_orange;
+                    }
+                    Marker marker = mMap.addMarker(new MarkerOptions()
+                            .position(place.getLatLng())
+                            .icon(BitmapDescriptorFactory.fromBitmap(
+                                    Bitmap.createScaledBitmap(((BitmapDrawable) getResources()
+                                            .getDrawable(markerResource)).getBitmap(), 80, 110, false)
+                            )));
+                    marker.setTag(place);
+                    markers.add(marker);
+                }).addOnFailureListener(this.onFailureListener(getFragmentContext().getResources().getString(R.string.afl_get_restaurant)));
+        }
+    }
+
+    /**
+     * Check if the restaurant passed as parameter have a marker on the map
+     * @param place restaurant
+     * @return true if exist
+     */
+    private boolean markerExist(Place place) {
+        for (Marker marker : markers) {
+            if (marker.getTag() == place) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onMarkerClick(@NonNull Marker marker) {
+        Place place = (Place) marker.getTag();
+        if (place != null) {
+            // get detail
+            return true;
+        }
+        return false;
+    }
 }
