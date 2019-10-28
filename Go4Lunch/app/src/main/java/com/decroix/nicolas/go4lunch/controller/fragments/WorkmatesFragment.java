09e85075449ef1fa1febb3f565416079a670b4bf
NDECROIX <nicolas.decroixx@gmail.com>
2019-10-28 21:20:05 +0100

    WorkmatesFragment : handle the click on a user

--- Go4Lunch/app/src/main/java/com/decroix/nicolas/go4lunch/controller/fragments/WorkmatesFragment.java
+++ Go4Lunch/app/src/main/java/com/decroix/nicolas/go4lunch/controller/fragments/WorkmatesFragment.java
@@ -1,9 +1,11 @@
 package com.decroix.nicolas.go4lunch.controller.fragments;
 
 
+import android.content.Context;
 import android.location.Location;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.DividerItemDecoration;
@@ -18,9 +20,13 @@ import com.decroix.nicolas.go4lunch.BuildConfig;
 import com.decroix.nicolas.go4lunch.R;
 import com.decroix.nicolas.go4lunch.api.UserHelper;
 import com.decroix.nicolas.go4lunch.base.BaseFragment;
+import com.decroix.nicolas.go4lunch.controller.activities.MainActivity;
 import com.decroix.nicolas.go4lunch.models.User;
 import com.decroix.nicolas.go4lunch.view.WorkmatesRecyclerViewAdapter;
 import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.net.FetchPhotoRequest;
+import com.google.android.libraries.places.api.net.FetchPlaceRequest;
 import com.google.android.libraries.places.api.net.PlacesClient;
 import com.google.firebase.firestore.DocumentSnapshot;
 
@@ -41,7 +47,10 @@ public class WorkmatesFragment extends BaseFragment implements WorkmatesRecycler
     RecyclerView recyclerView;
 
     private WorkmatesRecyclerViewAdapter adapter;
+    private PlacesClient placesClient;
     private List<User> users;
+
+    private Location myLocation;
 
     public WorkmatesFragment() {
         // Required empty public constructor
@@ -50,6 +59,8 @@ public class WorkmatesFragment extends BaseFragment implements WorkmatesRecycler
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Places.initialize(getFragmentContext(), BuildConfig.ApiKey);
+        placesClient = Places.createClient(getFragmentContext());
         users = new ArrayList<>();
     }
 
@@ -65,20 +76,10 @@ public class WorkmatesFragment extends BaseFragment implements WorkmatesRecycler
     }
 
     /**
-     * Configures the recycler view
-     */
-    private void configRecyclerView() {
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-        recyclerView.addItemDecoration(new DividerItemDecoration(getFragmentContext(), DividerItemDecoration.VERTICAL));
-        adapter = new WorkmatesRecyclerViewAdapter(this);
-        recyclerView.setAdapter(adapter);
-    }
-
-    /**
      * Get all users from firestore
      */
     private void getUsers() {
-        UserHelper.getUsers().addOnCompleteListener(task -> {
+        UserHelper.getUsers().addOnCompleteListener( task -> {
             if (task.isSuccessful() && task.getResult() != null) {
                 List<DocumentSnapshot> documents = task.getResult().getDocuments();
                 List<User> users = new ArrayList<>();
@@ -95,6 +96,16 @@ public class WorkmatesFragment extends BaseFragment implements WorkmatesRecycler
     }
 
     /**
+     * Configures the recycler view
+     */
+    private void configRecyclerView() {
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.addItemDecoration(new DividerItemDecoration(getFragmentContext(), DividerItemDecoration.VERTICAL));
+        adapter = new WorkmatesRecyclerViewAdapter(this);
+        recyclerView.setAdapter(adapter);
+    }
+
+    /**
      * Add all users in the recycler view
      * @param users users
      */
@@ -104,6 +115,41 @@ public class WorkmatesFragment extends BaseFragment implements WorkmatesRecycler
 
     @Override
     public void onClickUser(String restaurant) {
+        getPlaceDetails(restaurant);
+    }
 
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        myLocation = ((MainActivity) context).mLastKnownLocation;
+    }
+
+    /**
+     * Get details of the restaurant whose ID is passed in parameter
+     * @param id restaurant id
+     */
+    private void getPlaceDetails(String id) {
+
+        FetchPlaceRequest request = FetchPlaceRequest.newInstance(id, PLACE_FIELDS);
+
+        placesClient.fetchPlace(request).addOnSuccessListener(response -> {
+            Place place = response.getPlace();
+            if (place.getTypes() != null && place.getTypes().contains(Place.Type.RESTAURANT)) {
+                if (place.getPhotoMetadatas() != null) {
+                    FetchPhotoRequest photoRequest = FetchPhotoRequest.builder(place.getPhotoMetadatas().get(0))
+                            .setMaxWidth(500)
+                            .setMaxHeight(250)
+                            .build();
+
+                    placesClient
+                            .fetchPhoto(photoRequest).addOnSuccessListener(fetchPhotoResponse ->{
+                                // Start detail activity
+                            })
+                            .addOnFailureListener(this.onFailureListener(getString(R.string.afl_fetch_photo)));
+                } else {
+                    // Start detail activity
+                }
+            }
+        }).addOnFailureListener(this.onFailureListener(getString(R.string.afl_fetch_place)));
     }
 }
