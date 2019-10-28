94bfe650d404091cabd7464e4ed0eb16d12ec0fc
NDECROIX <nicolas.decroixx@gmail.com>
2019-10-28 19:48:42 +0100

    Inflate the view to the ListViewFragment and config RecyclerView

--- Go4Lunch/app/src/main/java/com/decroix/nicolas/go4lunch/controller/fragments/ListViewFragment.java
+++ Go4Lunch/app/src/main/java/com/decroix/nicolas/go4lunch/controller/fragments/ListViewFragment.java
@@ -3,19 +3,37 @@ package com.decroix.nicolas.go4lunch.controller.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.decroix.nicolas.go4lunch.R;
+import com.decroix.nicolas.go4lunch.base.BaseFragment;
+import com.decroix.nicolas.go4lunch.view.RestaurantRecyclerViewAdapter;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ListViewFragment extends Fragment {
+public class ListViewFragment extends BaseFragment {
 
+    @BindView(R.id.fragment_list_recycler_view)
+    RecyclerView recyclerView;
+    @BindView(R.id.fragment_list_restaurant_tv_is_empty)
+    TextView textViewIsEmpty;
+
+
+    private RestaurantRecyclerViewAdapter adapter;
+    private RestaurantRecyclerViewAdapter.OnClickRestaurantItemListener callback;
 
     public ListViewFragment() {
         // Required empty public constructor
@@ -23,10 +41,24 @@ public class ListViewFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list_view, container, false);
+        View view = inflater.inflate(R.layout.fragment_list_view, container, false);
+        ButterKnife.bind(this, view);
+        configRecyclerView();
+        textViewIsEmpty.setVisibility(View.INVISIBLE);
+        return view;
+    }
+
+    /**
+     * Configures the recycler view
+     */
+    private void configRecyclerView() {
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.addItemDecoration(new DividerItemDecoration(getFragmentContext(), DividerItemDecoration.VERTICAL));
+        adapter = new RestaurantRecyclerViewAdapter(callback);
+        recyclerView.setAdapter(adapter);
     }
 
 }
