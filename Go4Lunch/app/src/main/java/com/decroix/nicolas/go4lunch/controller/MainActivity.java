e27d0a4899c24f705885912f7952ca202422030c
NDECROIX <nicolas.decroixx@gmail.com>
2019-10-28 15:56:21 +0100

    Commit the empty main activity

--- Go4Lunch/app/src/main/java/com/decroix/nicolas/go4lunch/controller/MainActivity.java
+++ Go4Lunch/app/src/main/java/com/decroix/nicolas/go4lunch/controller/MainActivity.java
@@ -1,7 +1,10 @@
-package com.decroix.nicolas.go4lunch;
+package com.decroix.nicolas.go4lunch.controller;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+
+import com.decroix.nicolas.go4lunch.R;
 
 public class MainActivity extends AppCompatActivity {
 
