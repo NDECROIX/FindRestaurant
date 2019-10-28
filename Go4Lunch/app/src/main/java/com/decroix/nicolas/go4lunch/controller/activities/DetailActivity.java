ed4af95ccf183c397ee20ac6d828eb173a06122a
NDECROIX <nicolas.decroixx@gmail.com>
2019-10-28 22:38:28 +0100

    DetailActivity : Handle the click on the floating action button

--- Go4Lunch/app/src/main/java/com/decroix/nicolas/go4lunch/controller/activities/DetailActivity.java
+++ Go4Lunch/app/src/main/java/com/decroix/nicolas/go4lunch/controller/activities/DetailActivity.java
@@ -26,6 +26,7 @@ import com.decroix.nicolas.go4lunch.view.DetailActivityRecyclerViewAdapter;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.libraries.places.api.model.Place;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.List;
 
@@ -33,6 +34,7 @@ import javax.annotation.Nullable;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 import static com.decroix.nicolas.go4lunch.utils.UsefulFunctions.getDistance;
 import static com.decroix.nicolas.go4lunch.utils.UsefulFunctions.getOpeningHours;
@@ -187,5 +189,59 @@ public class DetailActivity extends BaseActivity {
                 break;
             }
         }
+    }
+
+    /**
+     * Handle the click on the floating action button.
+     */
+    @OnClick(R.id.activity_detail_fab)
+    public void onFabClick() {
+        addRestaurantForLunch();
+    }
+
+    /**
+     * Add the restaurant for lunch.
+     * Update the profile user in the database.
+     * Add the user in the users list.
+     * Delete the user from the last restaurant if exist
+     */
+    private void addRestaurantForLunch() {
+        if (!myUser.getLunchRestaurantID().equals(placeRestaurant.getId())){
+            if (myUser.getLunchRestaurantID() != null && !myUser.getLunchRestaurantID().isEmpty()) {
+                //Remove user from users list
+                RestaurantHelper.removeUserFromList(myUser.getLunchRestaurantID(), myUser)
+                        .addOnCompleteListener(task -> {
+                            // Update the user profile
+                            UserHelper.updateLunchRestaurant(FirebaseAuth.getInstance().getUid(), new Restaurant(placeRestaurant))
+                                    .addOnFailureListener(this.onFailureListener(getString(R.string.afl_update_lunch_restaurant)));
+                        }).addOnFailureListener(this.onFailureListener(getString(R.string.afl_remove_user_from_list)));
+            } else {
+                UserHelper.updateLunchRestaurant(FirebaseAuth.getInstance().getUid(), new Restaurant(placeRestaurant))
+                        .addOnFailureListener(this.onFailureListener(getString(R.string.afl_update_lunch_restaurant)));
+            }
+
+            // Check if the restaurant exist in the database else if create it.
+            RestaurantHelper.getRestaurant(placeRestaurant.getId()).addOnCompleteListener(snapshotTask -> {
+                if (snapshotTask.isSuccessful() && snapshotTask.getResult() != null) {
+                    addUserInWorkmateList();
+                } else {
+                    Restaurant newRestaurant = new Restaurant(this.placeRestaurant);
+                    RestaurantHelper.createRestaurant(newRestaurant).addOnCompleteListener(task1 -> addUserInWorkmateList());
+                }
+            }).addOnFailureListener(this.onFailureListener(getString(R.string.afl_get_restaurant)));
+        } else {
+            showMessage(getString(R.string.restaurant_already_added));
+        }
+    }
+    /**
+     * Add the currently user in the users list
+     */
+    private void addUserInWorkmateList() {
+        RestaurantHelper.addUserInList(placeRestaurant.getId(), myUser)
+                .addOnSuccessListener(result -> {
+                    fab.setImageResource(R.drawable.ic_check_circle_dark_24);
+                    getUserFromFirestore();
+                })
+                .addOnFailureListener(this.onFailureListener(getString(R.string.afl_add_user_in_list)));
     }
 }
