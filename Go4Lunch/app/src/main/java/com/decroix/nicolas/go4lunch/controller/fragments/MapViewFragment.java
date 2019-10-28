e99a2ab0386a780f24fc753add13de3807827e9f
NDECROIX <nicolas.decroixx@gmail.com>
2019-10-28 19:02:18 +0100

    Map view fragment : configuration of google map

--- Go4Lunch/app/src/main/java/com/decroix/nicolas/go4lunch/controller/fragments/MapViewFragment.java
+++ Go4Lunch/app/src/main/java/com/decroix/nicolas/go4lunch/controller/fragments/MapViewFragment.java
@@ -1,11 +1,14 @@
 package com.decroix.nicolas.go4lunch.controller.fragments;
 
 
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -18,6 +21,7 @@ import com.decroix.nicolas.go4lunch.base.BaseFragment;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.libraries.places.api.Places;
 import com.google.android.libraries.places.api.net.PlacesClient;
@@ -26,6 +30,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.ButterKnife;
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.EasyPermissions;
+
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -33,6 +41,9 @@ import butterknife.ButterKnife;
 public class MapViewFragment extends BaseFragment
         implements OnMapReadyCallback {
 
+
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 898;
+    private static final int DEFAULT_ZOOM = 18;
 
     private GoogleMap mMap;
     private PlacesClient placesClient;
@@ -65,6 +76,46 @@ public class MapViewFragment extends BaseFragment
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        updateLocationSetting();
+        updateMapStyle();
+    }
 
+    /**
+     * Update the style map with the style_json
+     */
+    private void updateMapStyle() {
+        mMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(getFragmentContext(), R.raw.style_json));
+    }
+
+    /**
+     * Updating the parameters of the mMap
+     */
+    @AfterPermissionGranted(PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION)
+    private void updateLocationSetting() {
+        mMap.getUiSettings().setMapToolbarEnabled(false);
+        mMap.getUiSettings().setMyLocationButtonEnabled(false);
+        if (EasyPermissions.hasPermissions(getFragmentContext(), ACCESS_FINE_LOCATION)) {
+            mMap.setMyLocationEnabled(true);
+        } else {
+            //mMap.setMyLocationEnabled(false);
+            getAccessFineLocationPermission();
+        }
+    }
+    /**
+     * Get the location permission.
+     */
+    private void getAccessFineLocationPermission() {
+        if (getActivity() != null && ContextCompat.checkSelfPermission(getFragmentContext(),
+                ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(),
+                    new String[]{ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
