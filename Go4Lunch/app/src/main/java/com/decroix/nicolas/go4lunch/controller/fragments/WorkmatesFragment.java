19c7f0361ebbbdef9623fc1699f3149388e05699
NDECROIX <nicolas.decroixx@gmail.com>
2019-10-28 21:02:16 +0100

    WorkmatesFragment : Inflate the layout and configure the RecyclerView.

--- Go4Lunch/app/src/main/java/com/decroix/nicolas/go4lunch/controller/fragments/WorkmatesFragment.java
+++ Go4Lunch/app/src/main/java/com/decroix/nicolas/go4lunch/controller/fragments/WorkmatesFragment.java
@@ -1,32 +1,69 @@
 package com.decroix.nicolas.go4lunch.controller.fragments;
 
 
+import android.location.Location;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.decroix.nicolas.go4lunch.R;
+import com.decroix.nicolas.go4lunch.base.BaseFragment;
+import com.decroix.nicolas.go4lunch.models.User;
+import com.decroix.nicolas.go4lunch.view.WorkmatesRecyclerViewAdapter;
+import com.google.android.libraries.places.api.net.PlacesClient;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+import static com.decroix.nicolas.go4lunch.api.UserHelper.getUsers;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class WorkmatesFragment extends Fragment {
+public class WorkmatesFragment extends BaseFragment implements WorkmatesRecyclerViewAdapter.OnClickUserListener {
 
+    @BindView(R.id.fragment_workmate_recycler_view)
+    RecyclerView recyclerView;
+
+    private WorkmatesRecyclerViewAdapter adapter;
 
     public WorkmatesFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_workmates, container, false);
+        View view = inflater.inflate(R.layout.fragment_workmates, container, false);
+        ButterKnife.bind(this, view);
+        //toolbarViewHolder = new ToolbarViewHolder(getActivity());
+        configRecyclerView();
+        getUsers();
+        return view;
     }
 
+    /**
+     * Configures the recycler view
+     */
+    private void configRecyclerView() {
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.addItemDecoration(new DividerItemDecoration(getFragmentContext(), DividerItemDecoration.VERTICAL));
+        adapter = new WorkmatesRecyclerViewAdapter(this);
+        recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onClickUser(String restaurant) {
+
+    }
 }
