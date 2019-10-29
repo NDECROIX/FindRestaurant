f54f4956bf8ff146964b0c43cd021ceada7e66e9
NDECROIX <nicolas.decroixx@gmail.com>
2019-10-29 18:18:21 +0100

    Test : Add class to test the navigation view

---
+++ Go4Lunch/app/src/androidTest/java/com/decroix/nicolas/go4lunch/utils/NavigationViewTestHelper.java
@@ -0,0 +1,34 @@
+package com.decroix.nicolas.go4lunch.utils;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+
+/**
+ * Useful for clicking on an item in the navigation view
+ */
+public class NavigationViewTestHelper {
+
+    public static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
