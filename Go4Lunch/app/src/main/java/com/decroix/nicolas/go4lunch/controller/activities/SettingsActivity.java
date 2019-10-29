8658ab970731f4379556d845f7cd3738dedea7ce
NDECROIX <nicolas.decroixx@gmail.com>
2019-10-29 15:54:40 +0100

    SettingActivity : Inflate the fragment and toolbar

--- Go4Lunch/app/src/main/java/com/decroix/nicolas/go4lunch/controller/activities/SettingsActivity.java
+++ Go4Lunch/app/src/main/java/com/decroix/nicolas/go4lunch/controller/activities/SettingsActivity.java
@@ -1,16 +1,41 @@
 package com.decroix.nicolas.go4lunch.controller.activities;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 import com.decroix.nicolas.go4lunch.R;
+import com.decroix.nicolas.go4lunch.controller.fragments.SettingsFragment;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    @BindView(R.id.settings_toolbar)
+    Toolbar settingsToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+        ButterKnife.bind(this);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.settings, new SettingsFragment())
+                .commit();
+
+        configToolbar();
+    }
+
+    private void configToolbar() {
+        setSupportActionBar(settingsToolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setTitle(getString(R.string.toolbar_title_settings));
+        }
     }
 }
